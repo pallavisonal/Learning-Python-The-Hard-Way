47f34ebbd4e6c82918b82757eec7bd6aa0fa5d82
Pallavi Sonal <sonalpallavi@gmail.com>
2016-05-08 11:26:41 +0530

    Added files via upload

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = "Pallavi Sonal";
+my_age = 35;
+my_height = 156
+my_weight = 45
+my_eyes = "black"
+my_teeth = "yellow"
+my_hair = "black"
+
+print ("Let's talk about %s" % my_name);
+print ("She is %d cms tall and %d kgs heavy." % ( my_height,my_weight ))
+print ("She has got %s eyes and %s hair. " %( my_eyes, my_hair))
+print ("Her teeth are mostly %s. " %(my_teeth))
+
+print ("If I add %d , %d, and %d I get %d." % (my_age, my_height, my_weight, my_height + my_weight + my_age))
+
+print (" Here I would like to try %r giving it my age" % my_age)

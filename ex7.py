2c86cd5432e3f475de1ea278d3630f5208010fe6
Pallavi Sonal <sonalpallavi@gmail.com>
2016-05-09 22:12:48 +0530

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,21 @@
+print ("Mary had a little lamb. ")
+print ("Its fleece is white as %s. " % 'snow')
+print ("And everywhere that Mary went. ")
+# print the character 10 times
+print ("-" * 10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+# I want the two print statements on same line - put a comma and end = " " , end is by default new line , here we are saying end will be space.  
+print (end1 + end2 + end3 + end4 + end5 + end6,  end = " ")
+print (end7 + end8 + end9 + end10 + end11 +end12 )

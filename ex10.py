a18aa209fd79e99de9f29028afa5401d5cd36a42
Pallavi Sonal <sonalpallavi@gmail.com>
2016-05-11 21:55:40 +0530

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,32 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I am \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
+
+single_cat = '''
+Hello dear "kitty"
+How do you do
+Isn't it hot today?
+'''
+print (single_cat)
+
+#while True:
+#   for i in ["/", "-", "|", "\\", "|"]:
+#	    print ("%s\r" %i,)
+#
+
+print ("hello %r." % "I am a \"singer\"")
+print ("\tIs this tabbed")
+print ("%r" % "\tIs this tabbed")
+print ("%s" % "\tIs this tabbed")

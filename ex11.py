8b003d9f39f8c73d301e79ce3e3dc801e64df2f9
Pallavi Sonal <sonalpallavi@gmail.com>
2016-05-12 14:24:55 +0530

    Taking user input

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print ("How old are you?", end = " ")
+age = input()
+print ("How tall are you?", end= " ")
+height = input()
+print ("How much do you weigh?", end = " ")
+weight = input()
+
+print ("So, you're %r years old, %r cms tall and %r kg heavy." % (age, height, weight))

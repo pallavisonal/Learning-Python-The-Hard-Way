f7cd6ffb60051bd36c850bfb2a323b0887b24d24
Pallavi Sonal <sonalpallavi@gmail.com>
2016-05-12 14:36:23 +0530

    Using input with prompts

---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How heavy are you? ")
+
+print ("So, you are %r years old, %r cms tall and %r kg heavy." % (
+     age, height, weight))
